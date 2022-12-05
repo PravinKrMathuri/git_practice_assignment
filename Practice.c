2973ebfa6e19c26aa7bda60f188b54bab91ac226
Pravin Kumar Mathuri <pravinkumarmathuri@gmail.com>
2022-12-05 23:21:41 +0530

    create star pattern

--- Practice.c
+++ Practice.c
@@ -13,11 +13,24 @@ int Divide(int a, int b)
     return a / b;
 }
 
+void StarPattern(int lines)
+{
+    for (int i = 0; i < lines; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+}
+
 int main()
 {
     int a = 5, b = 10;
     printf("Sum of %d and %d = %d", a, b, Sum(a, b));
     printf("\n Product of %d and %d = %d", a, b, product(a, b));
-    printf("\nDivide of %d and %d = %d", a, b, Divide(a, b));
+    printf("\nDivide of %d and %d = %d\n", a, b, Divide(a, b));
+    StarPattern(a);
     return 0;
 }
