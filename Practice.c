4a40f83600cb3ccb1566c8ca8f1aa0f53ddb45c2
Nitish <nitishkm.nk825@gmail.com>
2022-12-05 22:55:52 +0530

    Added one Divide function

--- Practice.c
+++ Practice.c
@@ -4,9 +4,14 @@ int Sum(int a,int b){
     return a+b;
 }
 
+int Divide(int a,int b){
+    return a/b;
+}
+
 int main() 
 {
-    int a=5,b=10;
+    int a=10,b=5;
     printf("Sum of %d and %d = %d",a,b,Sum(a,b));
+    printf("\nDivide of %d and %d = %d",a,b,Divide(a,b));
     return 0;
 }
