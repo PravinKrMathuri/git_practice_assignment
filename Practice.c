3e09a765649c6333354422d2dc854bfa105a932e
Pravin Kumar Mathuri <pravinkumarmathuri@gmail.com>
2022-12-05 22:50:59 +0530

    added another product function

--- Practice.c
+++ Practice.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 
-int Sum(int a,int b){
-    return a+b;
+int Sum(int a, int b)
+{
+    return a + b;
+}
+int product(int a, int b)
+{
+    return a * b;
 }
 
-int main() 
+int main()
 {
-    int a=5,b=10;
-    printf("Sum of %d and %d = %d",a,b,Sum(a,b));
+    int a = 5, b = 10;
+    printf("Sum of %d and %d = %d", a, b, Sum(a, b));
+    printf("\n Product of %d and %d = %d", a, b, product(a, b));
     return 0;
 }
