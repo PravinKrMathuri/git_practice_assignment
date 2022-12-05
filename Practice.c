6c2766748b94fa859d719c0a981ef4d95507bf10
Nitish <nitishkm.nk825@gmail.com>
2022-12-05 23:27:57 +0530

    Added one Subraction function

--- Practice.c
+++ Practice.c
@@ -13,11 +13,16 @@ int Divide(int a, int b)
     return a / b;
 }
 
+int Subtract(int a,int b){
+    return a-b;
+}
+
 int main()
 {
     int a = 5, b = 10;
     printf("Sum of %d and %d = %d", a, b, Sum(a, b));
     printf("\n Product of %d and %d = %d", a, b, product(a, b));
     printf("\nDivide of %d and %d = %d", a, b, Divide(a, b));
+    printf("\nDifference between %d and %d = %d",a,b,Subtract(a,b));
     return 0;
 }
